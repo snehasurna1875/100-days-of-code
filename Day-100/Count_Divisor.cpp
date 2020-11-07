56cde8a5d30378b88b6ababf9e16649b65bbd3cd
unknown <snehasurana1875@gmail.com>
2020-11-07 21:43:27 +0530

    100th Day

---
+++ Day-100/Count_Divisor.cpp
@@ -0,0 +1,13 @@
+#include<iostream>>
+using namespace std;
+int main(){
+	int l,r,k;
+	cin>>l>>r>>k;
+	int count=0;
+	for(int i=l;i<=r;i++){
+		if(i%k==0){
+			count=count+1;
+		}
+	}
+	cout<<count<<endl;
+}
